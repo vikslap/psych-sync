f9bb746ea42bf0b6afee5eb0eeff0d21943cea8d
Vikslap <souveekpal@gmail.com>
2026-05-07 23:29:40 +0530

    updated

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,3 +5,9 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log(
+  "Supabase URL Check:",
+  import.meta.env.VITE_SUPABASE_URL ? "Loaded" : "MISSING",
+);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
